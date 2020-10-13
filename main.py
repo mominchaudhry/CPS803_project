f231ac3747b862f0801131bf5157003745a10c7f
Momin Chaudhry <momin.chaudhry@ryerson.ca>
2020-10-13 10:51:13 -0400

    label extraction

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
+import numpy as np
 
 def load_dataset():
     rootDir = 'Images'
     for dirName, subdirList, fileList in os.walk(rootDir):
-        print('Found directory: %s' % dirName)
-        for fname in fileList:
-            print('\t%s' % fname)
+        #print('Found directory: %s' % dirName)
+        if dirName != 'Images':
+            l = dirName.split('-')
+            label = l[1]
+            print(label)
 
 def main():
     """ Main program """
