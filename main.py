bdbf5398133f9283a1c0122f194bb8ce685a2b99
Momin Chaudhry <momin.chaudhry@ryerson.ca>
2020-10-13 11:57:17 -0400

    load dataset

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
 
 def load_dataset():
     rootDir = 'Images'
+    images = np.zeros((20580, 500, 500, 3))
+    labels = np.empty((1), dtype=np.str)
+    i = 0
     for dirName, subdirList, fileList in os.walk(rootDir):
         #print('Found directory: %s' % dirName)
         if dirName != 'Images':
             l = dirName.split('-')
             label = l[1]
             print(label)
+        for fname in fileList:
+            print('\t%s' % fname)
+            if dirName != 'Images':
+                im = plt.imread(dirName + '/' + fname)
+                images[i] = im
+                i += 1
 
 def main():
     """ Main program """
