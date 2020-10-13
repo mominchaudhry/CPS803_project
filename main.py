f3ed6e1276407c7ff0b3bebc9769f79435024f8a
Momin Chaudhry <momin.chaudhry@ryerson.ca>
2020-10-13 10:36:26 -0400

    load data

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import os
+
+def load_dataset():
+    rootDir = 'Images'
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        print('Found directory: %s' % dirName)
+        for fname in fileList:
+            print('\t%s' % fname)
+
 def main():
     """ Main program """
     # Code goes over here.
     print("cyka!")
+    load_dataset()
     return 0
 
 if __name__ == "__main__":
