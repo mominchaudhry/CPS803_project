c3e5f33ad64af9ff2494422d9d71e9619698153d
mominchaudhryCB <momin.chaudhry@clearbridgemobile.com>
2020-11-07 11:36:58 -0500

    cropping dataset images by bounding box given in annotation files corresponding to each image (also added these annotation files, and went back to using rgb images instead of grayscaled, and reduced image sizes after cropping to 160x160)

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import re
+import time
 from PIL import Image
 
 
 
 def load_dataset():
     rootDir = 'Images'
-    images = np.zeros((1, 500, 500), dtype=np.uint8)
+    images = np.zeros((1, 160, 160, 3), dtype=np.uint8)
     labels = np.empty(20580, dtype=np.str)
     i = 0
     for dirName, subdirList, fileList in os.walk(rootDir):
@@ -16,15 +18,18 @@ def load_dataset():
         if dirName != 'Images':
             l = dirName.split('-')
             label = l[1]
+            print(label)
         for fname in fileList:
             if dirName != 'Images':
                 img = cv2.imread(dirName + '/' + fname)
-                im2 = ((img[:, :, 0] + img[:, :, 1] + img[:, :, 2]) / 3).astype(np.uint8)
-                im = cv2.resize(im2, dsize=(500, 500), interpolation=cv2.INTER_CUBIC)
-                im = np.reshape(im, (1, 500, 500))
+                bnd = [s.strip() for s in re.findall(r'<bndbox>(.*)</bndbox>', open("Annotation\\" + dirName.replace('Images\\', '') + "\\" + fname.replace('.jpg', '')).read(), re.DOTALL)][0].replace('<', ' ').replace('>', ' ').split()
+                bndbox = [bnd[1], bnd[4], bnd[7], bnd[10]]
+                im3 = img[int(bndbox[1]):int(bndbox[3]), int(bndbox[0]):int(bndbox[2]), :]
+                print(fname, i)
+                im = cv2.resize(im3, dsize=(160, 160), interpolation=cv2.INTER_CUBIC)
+                im = np.reshape(im, (1, 160, 160, 3))
                 images = np.append(images, im, axis=0)
                 labels[i] = label
-                print(i)
                 i += 1
 
 def main():
