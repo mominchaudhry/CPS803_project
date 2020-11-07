da6a2bbf244f9b912a3a113fb16c7856948f17d1
Hassaan Abbasi <hassaan.abbasi@ryerson.ca>
2020-11-07 13:53:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 
 
-def load_dataset():
+def load_dataset(verbose=False):
     rootDir = 'Images'
     images = np.zeros((1, 160, 160, 3), dtype=np.uint8)
     labels = np.empty(20580, dtype=np.str)
@@ -25,17 +25,18 @@ def load_dataset():
                 bnd = [s.strip() for s in re.findall(r'<bndbox>(.*)</bndbox>', open("Annotation\\" + dirName.replace('Images\\', '') + "\\" + fname.replace('.jpg', '')).read(), re.DOTALL)][0].replace('<', ' ').replace('>', ' ').split()
                 bndbox = [bnd[1], bnd[4], bnd[7], bnd[10]]
                 im3 = img[int(bndbox[1]):int(bndbox[3]), int(bndbox[0]):int(bndbox[2]), :]
-                #print(fname, i)
+                if verbose == True:
+                    print(fname, i)
                 im = cv2.resize(im3, dsize=(160, 160), interpolation=cv2.INTER_CUBIC)
                 im = np.reshape(im, (1, 160, 160, 3))
                 images = np.append(images, im, axis=0)
                 labels[i] = label
                 i += 1
+    return images[1:,:,:,:], labels
 
 def main():
     """ Main program """
     # Code goes over here.
-    print("cyka!")
     load_dataset()
     return 0
 
